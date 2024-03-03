1c37d366230e2b7bc877f0621623b4705be28e6a
hamza-abd-rahim <abdrahimhamza08@gmail.com>
2024-03-03 08:34:03 +0100

    'push__site'

--- src/layout/AccountLayout.tsx
+++ src/layout/AccountLayout.tsx
@@ -1,11 +1,18 @@
 import Navbar from "../pages/dashbord/navbar/Navbar";
 import user__background from '../assets/Rectangle 191.png';
 import user__img from '../assets/Ellipse 54.png';
-import styles from '../pages/userProfile/account.module.css';  // Assign the imported CSS module to a variable
+import styles from '../pages/userProfile/account.module.css';
 import { Link } from "react-router-dom";
 import UserInfo from "../pages/userProfile/components/UserInfo";
+import { useState } from "react";
+import EditUserInfo from "../pages/userProfile/components/EditUserInfo";
 
 const AccountLayout = () => {
+  const [isEditInfo, setEditInfo] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setEditInfo(!isEditInfo);
+  };
   const people = [
     { id: 1, name: "John", iconClass: "bi bi-person", customText: "Engineer" },
     { id: 2, name: "Jane", iconClass: "bi bi-person", customText: "Designer" },
@@ -24,11 +31,13 @@ const AccountLayout = () => {
           </div>
           <div className="flex-items px-2 px-lg-4 mt-2 justify-content-between flex-wrap gap-1">
             <p className="fw-medium fs-px_20 ln-ht-20px">Informations personels</p>
-            <Link to={""} className="medium-blue-button outline d-grid place-items-center">
+            <Link onClick={handleClick} to={""}
+             className={`${isEditInfo && 'outline'} medium-blue-button d-grid place-items-center`}>
               Modifire
             </Link>
           </div>
-          <div className="mt-3 d-grid gap-2">
+          {isEditInfo ? (
+            <div className="mt-3 animate__animated animate__zoomIn d-grid gap-2">
           {people.map(person => (
         <UserInfo
           key={person.id}
@@ -38,6 +47,10 @@ const AccountLayout = () => {
         />
       ))}
           </div>
+          ) : (
+            <EditUserInfo />
+          )}
+          
         </div>
       </div>
     </>
---
+++ src/pages/userProfile/components/EditUserInfo.tsx
@@ -0,0 +1,10 @@
+
+const EditUserInfo = () => {
+  return (
+    <div className="animate__animated animate__zoomIn">
+
+    </div>
+  )
+}
+
+export default EditUserInfo
